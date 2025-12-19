82212be23e4f5a8c2c38652619b5756868c272ff
Bill DeStein <bill@Bills-MacBook-Pro.local>
2025-12-19 05:11:39 -0500

    More work

--- src/AddRemove.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export function AddRemove(): React.JSX.Element {
-  return (
-    <div>AddRemove</div>
-  )
-}
-
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { AddRemove } from './AddRemove'
-import { Iframe } from './Iframe'
+import { AddRemove } from './examples/AddRemove'
+import { Iframe } from './examples/Iframe'
 
 type Example = {
   name: string
